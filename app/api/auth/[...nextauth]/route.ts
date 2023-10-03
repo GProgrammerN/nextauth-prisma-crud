d298f1303f04365790ab25fb741af8cf8006d4d3
GProgrammerN <guilherme.programador12@gmail.com>
2023-10-03 08:57:46 -0300

    :tada: Initial commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const handler = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? 'default',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? 'default'
+        })
+    ]
+
+})
+
+export { handler as GET, handler as POST }
